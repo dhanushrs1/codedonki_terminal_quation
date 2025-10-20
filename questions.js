d2308c1c004a11ae858a109a9d5be720b99c297f
Dhanush R.S <Dhanushrs013@gmail.com>
2025-10-21 00:09:22 +0530

    Add external Questions array with coding questions

---
+++ questions.js
@@ -0,0 +1,37 @@
+const externalQuestions = [
+  {
+    language: "Python",
+    question: "What will be the output of this code?",
+    code: 'print("Hello" + " " + "World")',
+    options: ["Hello World", "HelloWorld", "Error"],
+    correctIndex: 0
+  },
+  {
+    language: "JavaScript",
+    question: "What will be the output of this code?",
+    code: 'let x = 5;\nlet y = 3;\nconsole.log(x + y);',
+    options: ["8", "53", "undefined"],
+    correctIndex: 0
+  },
+  {
+    language: "Python",
+    question: "What will be the output of this code?",
+    code: 'for i in range(3):\n    print(i)',
+    options: ["0 1 2", "1 2 3", "Error"],
+    correctIndex: 0
+  },
+  {
+    language: "JavaScript",
+    question: "What will be the output of this code?",
+    code: 'let arr = [1, 2, 3];\nconsole.log(arr.length);',
+    options: ["3", "2", "undefined"],
+    correctIndex: 0
+  },
+  {
+    language: "Python",
+    question: "What will be the output of this code?",
+    code: 'name = "CodeDonki"\nprint(name.upper())',
+    options: ["CODEDONKI", "codedonki", "Error"],
+    correctIndex: 0
+  }
+];
